9664cb1dce6d5b1d2edb5854edb4b5c07679c49c
Drew Harris <harrisd@smu.edu>
2024-05-28 15:24:02 -0500

    feat: email org invitations

--- actix-server/src/handlers/mod.rs
+++ actix-server/src/handlers/mod.rs
@@ -1,3 +1,4 @@
 pub mod api_key_handler;
 pub mod auth_handler;
+pub mod invitation_handler;
 pub mod org_handler;
--- actix-server/src/operators/mod.rs
+++ actix-server/src/operators/mod.rs
@@ -1,4 +1,6 @@
 pub mod api_key_operator;
+pub mod email_operator;
+pub mod invitation_operator;
 pub mod org_operator;
 pub mod plan_operator;
 pub mod user_operator;
